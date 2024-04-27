655cc48958ac275323e82947ef20f038fec4f012
Puskar-Roy <puskarroy600@gmail.com>
2024-04-27 22:36:27 +0530

    feat : next-auth in web

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { RecoilProviders } from "../providers/RecoilProvider";
+import { SessionProviders } from "../providers/SessionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <RecoilProviders>{children}</RecoilProviders>
+        <RecoilProviders>
+          <SessionProviders>{children}</SessionProviders>
+        </RecoilProviders>
       </body>
     </html>
   );
